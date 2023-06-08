765902567217762bd7608ed45d4fb4e2d213df10
Kajetan Gulbinowicz <gulbinowicz997@gmail.com>
2023-06-08 15:24:18 +0200

    Add project files.

---
+++ Calculator.AspNetApp/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Calculator.AspNetApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+
+namespace Calculator.AspNetApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private DataTable _dataTable = new DataTable();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Result(string inputVal)
+        {
+            try
+            {
+                var result = Math.Round(Convert.ToDouble(_dataTable.Compute(inputVal.Replace(",","."),"")),2);
+
+                return Json(new { success = true, result });
+            }
+            catch(Exception exception) 
+
+            {
+                return Json(new { success = false, message = exception.Message });
+            }
+        }
+    }
+}
---
+++ Calculator.AspNetApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Calculator.AspNetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
